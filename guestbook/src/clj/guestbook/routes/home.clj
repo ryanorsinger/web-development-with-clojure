0e6effdfa79638a9980837c2202ba15ec34ee0a5
Ryan Orsinger <ryan.orsinger@gmail.com>
2022-05-02 15:14:30 -0500

    first commit

--- guestbook/src/clj/guestbook/routes/home.clj
+++ guestbook/src/clj/guestbook/routes/home.clj
@@ -13,10 +13,18 @@
 (defn about-page [request]
   (layout/render request "about.html"))
 
+(defn bio-page [request]
+  (layout/render request "bio.html"))
+
+(defn resume-page [request]
+  (layout/render request "resume.html"))
+
 (defn home-routes []
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
+   ["/bio" {:get bio-page}]
+   ["/resume" {:get resume-page}]
    ["/about" {:get about-page}]])
 
--- notes.md
+++ notes.md
@@ -10,3 +10,16 @@ control + enter evaluates the "current" form
 TAB auto-indents
 alt + delete overrides paredit's default to avoid deleting parens and brackets
 shift + option + control + C + Enter => 
+
+
+
+## Threading vs. Not Threading
+```clojure
+; threading approach
+(->> ["I" "💖" "Paredit"]
+     (interpose " ~ ")
+     (apply str))
+
+; non-threading approach
+(apply str (interpose " ~ " ["I" "💖" "Paredit"]))
+```
